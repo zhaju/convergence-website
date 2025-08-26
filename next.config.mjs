a0c2a887f1393315df6a3413c7cce845e29cc47d
zhaju <juleoan123@gmail.com>
2025-08-25 23:08:25 -0400

    maybe this will work

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/convergence-website',
-  assetPrefix: '/convergence-website/',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
